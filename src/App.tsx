ae6bfd90dd2f2897e6fd031b9156f45d88e09710
Rick <118506963+foxnaim@users.noreply.github.com>
2025-04-27 00:15:00 +0500

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Loading } from "./components/loading/Loading"; 
 import Home from "./pages/home/Home";

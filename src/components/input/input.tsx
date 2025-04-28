0772395d5e484f0d9fe3d4ebc579713585a44b9e
Rick <118506963+foxnaim@users.noreply.github.com>
2025-04-28 14:25:27 +0500

    modified:   src/components/input/input.tsx

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { Icons } from "../../ui/icons/Icons";
 import { InputProps } from "../../common.types";

ec6794fe5aa8b78f4eddfcf75b8eb6365c7a7c1c
Rick <118506963+foxnaim@users.noreply.github.com>
2025-04-29 11:52:53 +0500

    modified:   src/components/header/header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,8 +15,8 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className="w-full h-16 bg-gray-100 shadow-sm shadow-gray-400">
-      <div className="px-6 mx-auto py-2 flex justify-between items-center">
+    <div className=" w-full h-16 bg-gray-100 shadow-sm shadow-gray-400">
+      <div className="px-6 mx-auto py-3 flex justify-between items-center">
         <motion.h2
           className="font-bold"
           initial={{ opacity: 0 }}
@@ -30,17 +30,8 @@ const Header: React.FC = () => {
         </motion.h2>
 
         {/* Навигация для больших экранов */}
-        <nav className="hidden lg:block">
-          <motion.ul
-            className="flex gap-6"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            {ItemMenu.map((item, index) => (
-              <MenuItem key={index} name={item.title} link={item.link} />
-            ))}
-          </motion.ul>
+        <nav className="">
+         <Icons.user className="w-8 h-8 text-gray-600" />
         </nav>
 
         {/* Боковое меню для мобильных устройств */}
@@ -80,9 +71,18 @@ const Header: React.FC = () => {
               <div className="flex-1 p-4 ml-2">
                 <h3 className="text-lg font-bold text-gray-700 mb-2">Чаты</h3>
                 <ul className="space-y-4">
-                  <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer">Чат 1</li>
-                  <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer">Чат 2</li>
-                  <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer">Чат 3</li>
+                <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer flex justify-between items-center">
+                Чат 1
+                    <span><Icons.arrow className="size-4 text-gray-700"/></span>
+                  </li>
+                  <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer flex justify-between items-center">
+                  Чат 2
+                    <span><Icons.arrow className="size-4 text-gray-700"/></span>
+                  </li>
+                  <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer flex justify-between items-center">
+                  Чат 3
+                    <span><Icons.arrow className="size-4 text-gray-700"/></span>
+                  </li>
                   <li className="px-3 py-2 bg-gray-300 rounded-md cursor-pointer">Создать новый чат</li>
                 </ul>
               </div>

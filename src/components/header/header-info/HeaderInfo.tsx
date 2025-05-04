16e9a5412d08e4824ee22a48748214dbcce25e03
Rick <118506963+foxnaim@users.noreply.github.com>
2025-05-04 23:56:42 +0500

    modified:   src/components/header/header-info/HeaderInfo.tsx

--- src/components/header/header-info/HeaderInfo.tsx
+++ src/components/header/header-info/HeaderInfo.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-
-
 const HeaderInfo = () => {
   return (
-    <header className='w-full '>
-     <span>Nexora AI</span>
+    <header className="w-full px-10 my-5 h-16">
+      <div className="px-5 h-16 rounded-xl flex justify-between items-center shadow-2xl">
+        <span className="text-3xl font-semibold">Nexora AI</span>
+        <nav>
+          <ul className="flex space-x-6 text-gray-600">
+            <li>
+              <a
+                href="#about-us"
+                className="transition-colors duration-300 hover:text-green-600"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#how-to-use"
+                className="transition-colors duration-300 hover:text-green-600"
+              >
+                How to use
+              </a>
+            </li>
+            <li>
+              <a
+                href="#problem-solving"
+                className="transition-colors duration-300 hover:text-green-600"
+              >
+                Problem solving
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default HeaderInfo
+export default HeaderInfo;

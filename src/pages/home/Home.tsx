e4a2883fb685449594385551db3b8c47bc743b88
Rick <118506963+foxnaim@users.noreply.github.com>
2025-05-05 01:34:25 +0500

    modified:   package.json 	deleted:    src/components/Three/ThreeCircle.tsx 	modified:   src/pages/home/Home.tsx 	modified:   yarn.lock

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import HeaderInfo from "../../components/header/header-info/HeaderInfo";
-import ThreePieChart from "../../components/Three/ThreeCircle";
 
 const Home = () => {
   return (
@@ -13,24 +12,21 @@ const Home = () => {
             </h1>
             <p className="text-lg my-5">
               С помощью Nexora AI вы узнаете, какие ниши растут, где рынок
-              перенасыщен, а где есть неиспользованные возможности. Мы используем
-              большие данные и машинное обучение, чтобы дать вам чёткие
-              рекомендации для запуска успешного бизнеса.
+              перенасыщен, а где есть неиспользованные возможности. Мы
+              используем большие данные и машинное обучение, чтобы дать вам
+              чёткие рекомендации для запуска успешного бизнеса.
             </p>
             <button
               type="button"
               className="bg-green-200 hover:bg-green-300 py-2 px-6 rounded-lg border-2 border-gray-300 duration-200"
             >
-              Join
+              Присоединиться
             </button>
-            <aside className="md:w-1/2 mt-10 md:mt-0 flex justify-center items-center">
-  <div className="w-72 h-72 rounded-lg shadow-inner">
-    <ThreePieChart />
-  </div>
-</aside>
-            </article>
+          </article>
+          <aside className="md:w-1/2 mt-10 md:mt-0 flex justify-center items-center">
+           ХЪиолриолри
+          </aside>
         </section>
-
       </main>
     </div>
   );
